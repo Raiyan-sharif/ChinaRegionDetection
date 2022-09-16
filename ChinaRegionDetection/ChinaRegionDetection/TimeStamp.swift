e704bd43430bf3817412b16aa57ad3c3672738bf
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2022-09-16 09:31:29 +0600

    China Mode from timestamp

--- ChinaRegionDetection/ChinaRegionDetection/TimeStamp.swift
+++ ChinaRegionDetection/ChinaRegionDetection/TimeStamp.swift
@@ -18,18 +18,38 @@ class TimeStamp {
     var timeZoneIdentifiers: [String] { return TimeZone.knownTimeZoneIdentifiers }
     
     func printTimeZone() {
-        print(localTimeZoneIdentifier)
-        print(localTimeZoneDescription)
-        print(localTimeZoneAbbreviation)
-        print(timeZoneAbbreviations)
-        print(timeZoneIdentifiers)
+//        print(localTimeZoneIdentifier)
+//        print(localTimeZoneDescription)
+//        print(localTimeZoneAbbreviation)
+//        print(timeZoneAbbreviations)
+//        print(timeZoneIdentifiers)
 
         let locale = Locale.current
-        let unsortedCountries = Locale.isoRegionCodes.map {   locale.localizedString(forRegionCode: $0)! }
-        let  sortedCountries = unsortedCountries.sorted() as NSArray
-        print(sortedCountries) // list of countries
-        print(locale.regionCode)
-        print(locale.localizedString(forRegionCode: locale.regionCode!))
+//        let unsortedCountries = Locale.isoRegionCodes.map {   locale.localizedString(forRegionCode: $0)! }
+        let china_region = TimeZone.abbreviationDictionary
+        
+        let regionDictionary: [String] = ["Asia/Shanghai", "Asia/Urumqi", "Asia/Harbin", "Asia/Chongqing", "Asia/Kashgar"]
+//        print(china_region)
+//        print(locale.calendar.identifier)
+        let cnLanguage = Locale(identifier: "en_US_POSIX").localizedString(forLanguageCode: "zh")
+//        for i in TimeZone.knownTimeZoneIdentifiers{
+//            if(cnLanguage == "Chinese"){
+            if(regionDictionary.contains(TimeZone.autoupdatingCurrent.identifier)){
+                print("\(TimeZone.autoupdatingCurrent.identifier) Inside China")
+            }
+            else{
+                print("\(TimeZone.autoupdatingCurrent.identifier) Outside China")
+            }
+//            }
+//        }
+        print(localTimeZoneIdentifier)
+        print(TimeZone.autoupdatingCurrent.identifier)
+        print(TimeZone.knownTimeZoneIdentifiers.contains(where: { $0 == "Asia/Shanghai"
+        }))
+//        let  sortedCountries = unsortedCountries.sorted() as NSArray
+//        print(sortedCountries) // list of countries
+        //print(locale.regionCode)
+//        print(locale.localizedString(forRegionCode: locale.regionCode!))
     }
 
     func isUserInChina() -> Bool {
